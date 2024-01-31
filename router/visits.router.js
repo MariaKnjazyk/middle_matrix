ace923ee623954bfab20daf9f94ad08d8cf44ef5
kniazyk_m <mariia.kniazyk@inoxoft.com>
2024-01-31 19:05:36 +0200

    everything for which there was enough time

--- router/visits.router.js
+++ router/visits.router.js
@@ -7,7 +7,7 @@ const { enums: { USER_ROLES } } = require('../configs')
 
 router.route('/')
     .get(celebrate(visitValidator.getList), visitController.getTimeSlots)
-    .post(celebrate(visitValidator.create), authMiddleware.validateToken(),  userMiddleware.checkUserAccess([USER_ROLES.DOCTOR, USER_ROLES.HEAD_DOCTOR]), visitMiddleware.isTimeSlotNotFree, visitController.)
+    .post(celebrate(visitValidator.create), authMiddleware.validateToken(),  userMiddleware.checkUserAccess([USER_ROLES.DOCTOR, USER_ROLES.HEAD_DOCTOR]), visitMiddleware.isTimeSlotNotFree, visitController.create);
 
 
 module.exports = router;
